5d0fb6f041449cf4c6649e861093335551ccb073
gultaczaur <gultaczaur@gmail.com>
2025-01-19 12:06:06 +0400

    first

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { Provider } from 'react-redux'
-import { store } from './assets/redux/app/store.js'
+import  {store}  from './assets/redux/app/store.js'
 
 createRoot(document.getElementById('root')).render(
     <Provider store={store}>
-    <App />
+        <App />
     </Provider>
 )
